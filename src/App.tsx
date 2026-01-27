eb36457e462d88028cdb057f6202d300ec2700a5
Juan David Merchan Torres <juan1014166@gmail.com>
2026-01-27 15:11:48 -0500

    feat(App): Add CustomersList to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState, type JSX } from 'react';
 
 import type { CreateCustomerPayload, Customer } from './types';
 import { CustomerForm } from './components/CustomerForm';
+import { CustomersList } from './components/CustomersList';
+import { CustomersListItem } from './components/CustomersListItem';
 import { Header } from './components/Header';
 
 export function App(): JSX.Element {
@@ -25,6 +27,15 @@ export function App(): JSX.Element {
       <Header />
       <main className="m-auto flex max-w-5xl gap-8">
         <CustomerForm onAdd={queueAppend} />
+        <CustomersList queue={queue}>
+          {queue.map((service) => (
+            <CustomersListItem
+              key={service.id}
+              customer={)service}
+              onRemove={queueRemove}
+            />
+          ))}
+        </CustomersList>
       </main>
     </>
   );
