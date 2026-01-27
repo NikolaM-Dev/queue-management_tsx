e7dc7e7d0425c4c82dfdeaf0463062c322497db6
Juan David Merchan Torres <juan1014166@gmail.com>
2026-01-27 10:35:16 -0500

    feat(App): Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,14 @@
+import { type JSX } from 'react';
 
+export function App(): JSX.Element {
+  return (
+    <>
+      <header className="py-20 text-center">
+        <h1 className="mb-3 text-4xl font-bold text-violet-400">
+          Queue Management Application
+        </h1>
+        <p className="text-gray-400">Manage your customers efficiently</p>
+      </header>
+    </>
+  );
+}
