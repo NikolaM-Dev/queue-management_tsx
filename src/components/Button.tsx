b79a202331a25dfd0e116aa411dbdc7a1b171647
Juan David Merchan Torres <juan1014166@gmail.com>
2026-01-27 15:09:49 -0500

    feat(components): Add Button Component

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import type { JSX } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type Props = {
+  accent: string;
+  children: React.ReactNode;
+  label: string;
+  onClick: () => void;
+};
+
+export function Button({
+  accent,
+  label,
+  onClick,
+  children,
+}: Props): JSX.Element {
+  return (
+    <button
+      className={twMerge(
+        'text-md flex cursor-pointer items-center gap-1 rounded-md px-4 py-1 text-neutral-900 hover:opacity-90',
+        accent,
+      )}
+      onClick={onClick}
+      type="button"
+    >
+      {children}
+      {label}
+    </button>
+  );
+}
