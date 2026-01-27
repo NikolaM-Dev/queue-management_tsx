099e4b58b74207fbe3e4f11997da59c63e0eaf7c
Juan David Merchan Torres <juan1014166@gmail.com>
2026-01-27 15:04:48 -0500

    feat(App): Add CustomerForm to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import { type JSX } from 'react';
+import { useState, type JSX } from 'react';
 
+import type { CreateCustomerPayload, Customer } from './types';
+import { CustomerForm } from './components/CustomerForm';
 import { Header } from './components/Header';
 
 export function App(): JSX.Element {
+  const [queue, setQueue] = useState<Customer[]>([]);
+
+  function queueAppend(payload: CreateCustomerPayload): void {
+    setQueue((prev) =>
+      prev.concat({
+        ...payload,
+        id: new Date().getTime(),
+        status: 'waiting',
+      }),
+    );
+  }
+  function queueRemove(id: number): void {
+    setQueue((prev) => prev.filter((service) => service.id !== id));
+  }
+
   return (
     <>
       <Header />
+      <main className="m-auto flex max-w-5xl gap-8">
+        <CustomerForm onAdd={queueAppend} />
+      </main>
     </>
   );
 }
