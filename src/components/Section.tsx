cf66c583316d5424f8d86743fe9ac2691914bf4b
Juan David Merchan Torres <juan1014166@gmail.com>
2026-01-27 15:02:14 -0500

    feat(components): Add Section and SectionTitle component

---
+++ src/components/Section.tsx
@@ -0,0 +1,17 @@
+import type { JSX } from 'react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function Section({ children }: Props): JSX.Element {
+  return (
+    <section className="rounded-md bg-neutral-800 p-8">{children}</section>
+  );
+}
+
+function SectionTitle({ children }: Props): JSX.Element {
+  return <h2 className="text-md mb-4 font-bold text-violet-300">{children}</h2>;
+}
+
+export { Section, SectionTitle };
