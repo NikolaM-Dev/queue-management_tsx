b6b3e0f9b865b11c7fe5d668c9169bbefef10dcf
Juan David Merchan Torres <juan1014166@gmail.com>
2026-01-27 11:27:52 -0500

    refactor(App): Change to use the header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import type { JSX } from 'react';
+
+export function Header(): JSX.Element {
+  return (
+    <header className="py-20 text-center">
+      <h1 className="mb-3 text-4xl font-bold text-violet-400">
+        Queue Management Application
+      </h1>
+      <p className="text-gray-400">Manage your customers efficiently</p>
+    </header>
+  );
+}
